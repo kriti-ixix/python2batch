5657ffdffe1608829a34b68e1dffc9d9dc2d5410
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-05-21 16:25:50 +0530

    Update NewClasses.py

--- NewClasses.py
+++ NewClasses.py
@@ -1,10 +1,9 @@
+#Parent/Base class
 class StudentInfo():
-	def __init__(self, newName="N/A", newRollno=0, newGender=""):
+	def __init__(self, newName, newRollno, newGender):
 		self.rollno = newRollno
 		self.name = newName
-		self.marks = 0
 		self.gender = newGender
-		self.percentage = 0
 		self.subjects = []
 
 	def calculatePercentage(self):
@@ -14,6 +13,25 @@ class StudentInfo():
 		return str(self.rollno) + ". " + self.name
 
 
+#Child/sub class
+class Marks(StudentInfo):
+	def __init__(self, newMarks, newPercent, newSub, newName, newRollno, newGender):
+		StudentInfo.__init__(self, newName, newRollno, newGender)
+		self.marks = newMarks
+		self.percentage = newPercent
+		self.subjectName = newSub
+
+	def __str__(self):
+		return self.name + " got " + str(self.marks) + " in " + self.subjectName
+
+
+subject1 = Marks(90, 90, "English", "ABC", 5, "F")
+print(subject1)
+
+
+
+
+'''
 student1 = StudentInfo("Kriti", 123, "F")
 student2 = StudentInfo("Sharan", 456, "F")
 student3 = StudentInfo("XYZ", 345, "M")
@@ -24,6 +42,4 @@ print(student1)
 print(student2)
 print(student3)
 print(student4)
-
-#print(student1.rollno, ".", student1.name, student1.gender)
-#print(student2.rollno, ".", student2.name, student2.gender)
+'''
